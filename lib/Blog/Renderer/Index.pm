95d99f7fd8ee0fad61bfbb14cdef8949b0af3f4c
mufti <iwak.sotong@gmail.com>
2017-04-15 11:06:36 +0700

    compress resulting HTML, update styles

--- lib/Blog/Renderer/Index.pm
+++ lib/Blog/Renderer/Index.pm
@@ -6,25 +6,44 @@ use Template::Semantic;
 use Cpanel::JSON::XS;
 use DateTime::Format::Strptime;
 use Blog::Post;
+use HTML::Packer;
 
 sub load_index
 {
   my ($self, $filename) = @_;
+  my $date_format = DateTime::Format::Strptime->new(
+    pattern   => '%d/%m/%Y, %H:%M',
+    locale    => 'id_ID',
+    time_zone => 'Asia/Jakarta'
+  );
 
   my $json_file = path($filename);
   my $json_index = $json_file->slurp_utf8;
   my $index = decode_json $json_index;
 
-  return $index;
+  my @index_date_parsed = map {
+    my $item = $_;
+    $item->{created_at_dt} = $date_format->parse_datetime($item->{created_at});
+    $item;
+  } @{$index};
+
+  return \@index_date_parsed;
 }
 
 sub add_to_index
 {
   my ($self, $post, $index) = @_;
+  my $date_format = DateTime::Format::Strptime->new(
+    pattern   => '%d/%m/%Y, %H:%M',
+    locale    => 'id_ID',
+    time_zone => 'Asia/Jakarta'
+  );
+
 
   push @{$index}, {
     title      => $post->{title},
     created_at => $post->{created_at},
+    created_at_dt => $date_format->parse_datetime($post->{created_at}),
     slug       => $post->{slug}
   } unless grep { $_->{slug} eq $post->{slug} } @{$index};
 }
@@ -33,15 +52,9 @@ sub sort_index
 {
   my ($self, $index) = @_;
 
-  my $datetime_parser = DateTime::Format::Strptime->new(
-    pattern   => '%d/%m/%Y, %H:%M',
-    locale    => 'id_ID',
-    time_zone => 'Asia/Jakarta'
-  );
-
   my $sorter = sub {
-    my $first = $datetime_parser->parse_datetime($a->{created_at});
-    my $second = $datetime_parser->parse_datetime($b->{created_at});
+    my $first = $a->{created_at_dt};
+    my $second = $b->{created_at_dt};
 
     return $second->compare($first);
   };
@@ -52,6 +65,11 @@ sub sort_index
 sub write_json_index
 {
   my ($self, $filename, $data) = @_;
+
+  foreach my $item (@{$data}) {
+    delete $item->{created_at_dt};
+  }
+
   my $json_index_file = path($filename);
   $json_index_file->spew(encode_json $data);
 }
@@ -60,7 +78,7 @@ sub render_index_item
 {
   my ($self, $data) = @_;
   return {
-    '.post-index__item-timestamp' => \$data->{created_at},
+    '.post-index__item-timestamp' => \($data->{created_at_dt}->ymd('/')),
     '.post-index__item-title'     => {
       'a'      => $data->{title},
       'a@href' => ('/' . $data->{slug})
@@ -73,22 +91,28 @@ sub render_index
   my ($self, $data) = @_;
   my @index_items = map { $self->render_index_item($_); } @{$data};
 
-  my $index_html = Template::Semantic->process('src/views/index.html', {
+  my $index_html_uncompressed = Template::Semantic->process('src/views/index.html', {
       '.post-index .post-index__item' => [@index_items]
+  })->as_string;
+
+  my $packer = HTML::Packer->init();
+  my $index_html = $packer->minify(\$index_html_uncompressed, {
+      remove_newlines => 1,
+      remove_comments => 1
   });
 
-  path('public/index.html')->spew($index_html);
+  path('../public/index.html')->spew_utf8($index_html);
 }
 
 sub render
 {
   my ($self, $data) = @_;
-  my $index = $self->load_index('public/index.json');
+  my $index = $self->load_index('../public/index.json');
   $self->add_to_index($data, $index);
 
   my @sorted_index = $self->sort_index($index);
-  $self->write_json_index('public/index.json', \@sorted_index);
   $self->render_index(\@sorted_index);
+  $self->write_json_index('../public/index.json', \@sorted_index);
 }
 
 1;
--- lib/Blog/Renderer/Post.pm
+++ lib/Blog/Renderer/Post.pm
@@ -3,19 +3,27 @@ use strict;
 use warnings;
 use Path::Tiny 'path';
 use Template::Semantic;
+use HTML::Packer;
 
 sub render
 {
   my ($self, $blog_post) = @_;
-  my $rendered = Template::Semantic->process('src/views/post.html', {
-      'title, h2' => $blog_post->title,
+  my $rendered_uncompressed = Template::Semantic->process('src/views/post.html', {
+      'title' => $blog_post->title,
+      '.post__title' => $blog_post->title,
       'div.post__timestamp' => $blog_post->created_at,
       'div.post__content' => \$blog_post->body
+  })->as_string;
+
+  my $packer = HTML::Packer->init();
+  my $rendered = $packer->minify(\$rendered_uncompressed, {
+      remove_newlines => 1,
+      remove_comments => 1
   });
 
-  my $path = path("public/" . $blog_post->slug);
+  my $path = path("../public/" . $blog_post->slug);
   $path->mkpath;
-  $path->child('index.html')->spew($rendered);
+  $path->child('index.html')->spew_utf8($rendered);
 }
 
 1;
